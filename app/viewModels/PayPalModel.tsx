b29c2b9bb948f7b285f2ce499a5b16cecbaa1c7f
Abhishek Garg <abhishek.garg@successive.tech>
2021-12-02 20:01:24 +0530

    Final PayPal Integration

---
+++ app/viewModels/PayPalModel.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import PayPalScreen from "../views/PayPalScreen/PayPalScreen";
+
+const PayPalModel = () => {
+    return (
+        <PayPalScreen />
+    )
+}
+export default PayPalModel;
